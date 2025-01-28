5447bbd4cc2c0a10768a19f0a85854a01df695a3
Mohammad Ramjan Miya <miyaramjan93@gmail.com>
2025-01-28 14:43:12 +0545

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/map_ui_for_hazam_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.map_ui_for_hazam_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:map_ui_for_hazam_project/custom_info_windows.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Location(),
+    );
+  }
+}
